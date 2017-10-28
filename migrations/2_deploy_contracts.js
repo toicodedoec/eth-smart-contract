edd222ddaa62f32e1b39cc9f13e235a6a4c5ca25
doannx <doannx@gmail.com>
2017-10-28 21:17:30 +0700

    Update deployment script.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,27 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+const IcoToken = artifacts.require('IcoToken');
+const IcoContract = artifacts.require('IcoContract');
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(
+    IcoToken,
+    'Test Token',
+    'TST',
+    '18',
+    '1.0'
+  ).then(() => {
+    return deployer.deploy(
+      IcoContract,
+      '0x0b978c4d26a2acfedc96ca9bff531d6c5608effb', // Your ETH Address
+      IcoToken.address,
+      '100000000000000000000000000', // 100000000 Token
+      '1000', // 1 ETH = 1000 Token
+      '1504051200', // 30/08/2017
+      '1514592000', // 30/12/2017
+      '100000000000000000' // 0.1 ETH
+    ).then(() => {
+      return IcoToken.deployed().then(function(instance) {
+        return instance.setIcoContract(IcoContract.address);
+      });
+    });
+  });
 };
